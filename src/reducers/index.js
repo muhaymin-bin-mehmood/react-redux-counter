541447a4c6a7e7b50d92274b34680005def2c86b
Muhaymin Bin Mehmood <72308336+muhaymin-bin-mehmood@users.noreply.github.com>
2021-08-31 01:41:09 +0500

    'react-redux-counter'

---
+++ src/reducers/index.js
@@ -0,0 +1,9 @@
+import changeTheNumber from "./reducers";
+
+import { combineReducers } from "redux";
+
+const rootReducer = combineReducers({
+    changeTheNumber
+});
+
+export default rootReducer
