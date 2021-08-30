5748eadc254753c013861b7e5231d9e1e4847f93
Muhaymin Bin Mehmood <72308336+muhaymin-bin-mehmood@users.noreply.github.com>
2021-08-31 01:52:30 +0500

    Updates

--- src/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import changeTheNumber from "./reducers";
-
-import { combineReducers } from "redux";
-
-const rootReducer = combineReducers({
-    changeTheNumber
-});
-
-export default rootReducer
